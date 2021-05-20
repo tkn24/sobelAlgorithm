32af06b1e5b0b15955a940736955cf72dd250c88
ilker Tekin <ilkertekin_24@hotmail.com>
2021-05-20 12:10:48 +0300

    Add project files.

---
+++ sobell/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(124, 211)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 46)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Choose Picture"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(67, 22)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(219, 183)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox2.Location = New System.Drawing.Point(344, 22)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(219, 183)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(393, 211)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(120, 46)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Sobel Apply"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(618, 270)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Sobel Algorithm"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button3 As Button
+End Class
---
+++ sobell/Form1.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form1
+    Dim picture As New OpenFileDialog()
+    Dim image, grey, sobel, buffer As Bitmap
+    Dim i, j, value, r3, r4, valX, valY, gradient As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Dim GX(3, 3) As Integer
+    Dim GY(3, 3) As Integer
+    Dim color As Color
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        picture.Filter = "File Type|*.jpg;*.png;*.bmp"
+        picture.Title = "Choose Picture"
+        If picture.ShowDialog() <> System.Windows.Forms.DialogResult.OK Then
+            Return
+        End If
+        PictureBox1.ImageLocation = picture.FileName
+        Button3.Enabled = True
+    End Sub
+
+    Function makeGrey(ByVal bmp As Bitmap) As Bitmap
+
+        For i = 0 To bmp.Height - 1
+            For j = 0 To bmp.Width - 1
+                color = bmp.GetPixel(j, i)
+                r3 = color.G
+                r4 = color.B
+                value = (bmp.GetPixel(j, i).R + r3 + r4) / 3
+                bmp.SetPixel(j, i, Color.FromArgb(bmp.GetPixel(j, i).A, value, value, value))
+            Next
+        Next
+        Return bmp
+    End Function
+
+    Function applySobel(ByVal image As Bitmap) As Bitmap
+        grey = makeGrey(image)
+        buffer = New Bitmap(grey.Width, grey.Height)
+
+        GX(0, 0) = -1
+        GX(0, 1) = 0
+        GX(0, 2) = 1
+
+        GX(1, 0) = -2
+        GX(1, 1) = 0
+        GX(1, 2) = 2
+
+        GX(2, 0) = -1
+        GX(2, 1) = 0
+        GX(2, 2) = 1
+
+        GY(0, 0) = -1
+        GY(0, 1) = -2
+        GY(0, 2) = -1
+
+        GY(1, 0) = 0
+        GY(1, 1) = 0
+        GY(1, 2) = 0
+
+        GY(2, 0) = 1
+        GY(2, 1) = 2
+        GY(2, 2) = 1
+        For i = 0 To grey.Height - 1
+            For j = 0 To grey.Width - 1
+
+                If i = 0 Or i = grey.Height - 1 Or j = 0 Or j = grey.Width - 1 Then
+                    color = Color.FromArgb(255, 255, 255)
+                    buffer.SetPixel(j, i, color)
+                    valX = 0
+                    valY = 0
+                Else
+                    valX = grey.GetPixel(j - 1, i - 1).R * GX(0, 0) +
+                          grey.GetPixel(j, i - 1).R * GX(0, 1) +
+                          grey.GetPixel(j + 1, i - 1).R * GX(0, 2) +
+                          grey.GetPixel(j - 1, i).R * GX(1, 0) +
+                          grey.GetPixel(j, i).R * GX(1, 1) +
+                          grey.GetPixel(j + 1, i).R * GX(1, 2) +
+                          grey.GetPixel(j - 1, i + 1).R * GX(2, 0) +
+                          grey.GetPixel(j, i + 1).R * GX(2, 1) +
+                          grey.GetPixel(j + 1, i + 1).R * GX(2, 2)
+
+                    valY = grey.GetPixel(j - 1, i - 1).R * GY(0, 0) +
+                         grey.GetPixel(j, i - 1).R * GY(0, 1) +
+                         grey.GetPixel(j + 1, i - 1).R * GY(0, 2) +
+                         grey.GetPixel(j - 1, i).R * GY(1, 0) +
+                         grey.GetPixel(j, i).R * GY(1, 1) +
+                         grey.GetPixel(j + 1, i).R * GY(1, 2) +
+                         grey.GetPixel(j - 1, i + 1).R * GY(2, 0) +
+                         grey.GetPixel(j, i + 1).R * GY(2, 1) +
+                         grey.GetPixel(j + 1, i + 1).R * GY(2, 2)
+
+                    gradient = Int(Math.Abs(valX) + Math.Abs(valY))
+                    If gradient < 0 Then
+                        gradient = 0
+                    End If
+                    If gradient > 255 Then
+                        gradient = 255
+                    End If
+
+                    buffer.SetPixel(j, i, Color.FromArgb(gradient, gradient, gradient))
+
+                End If
+            Next
+        Next
+        Return buffer
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        image = New Bitmap(PictureBox1.Image)
+        sobel = applySobel(image)
+        PictureBox2.Image = sobel
+    End Sub
+
+
+
+End Class
